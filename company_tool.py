1096109d4ea02e705c761e33452547f4446c92f8
sajad.tlpr <123149691+sajadtlpr@users.noreply.github.com>
2023-09-29 22:27:39 +0530

    Update company_tool.py

--- company_tool.py
+++ company_tool.py
@@ -1,64 +1,48 @@
-# company_tool.py
-
 import os
+import time
 
 class CompanyInfo:
 
-  def __init__(self, name, details, founders, location, developers, ascii_art):
-    self.name = name
-    self.details = details
-    self.founders = founders
-    self.location = location
-    self.developers = developers
-    self.ascii_art = ascii_art
+    def __init__(self, name, details, founders, location, developers, ascii_art):
+        self.name = name
+        self.details = details
+        self.founders = founders
+        self.location = location
+        self.developers = developers
+        self.ascii_art = ascii_art
 
-  def display_info(self):
-
-    # Center ASCII art in "div"
-    print("" TLPRORG "
-   
-
-    # Format details
-    print("Company Name: " + self.name)
-    print()    
-    print("Overview:")
-    print(self.details)
-    print()   
-    print("Founders: " + ', '.join(self.founders))
-    print("Location: " + self.location)
-    print("Developers: " + ', '.join(self.developers))
+    def display_info(self):
+        os.system('clear')  # Clear the terminal screen
+        print(f"\033[92m{self.ascii_art}\033[0m".center(os.get_terminal_size().columns))
+        print(f"\033[96m{self.name}\033[0m".center(os.get_terminal_size().columns))
+        print(f"\033[96m{self.details}\033[0m".center(os.get_terminal_size().columns))
+        print(f"\033[91mFounders: {', '.join(self.founders)}\033[0m".center(os.get_terminal_size().columns))
+        print(f"\033[91mLocation: {self.location}\033[0m".center(os.get_terminal_size().columns))
+        print(f"\033[91mDevelopers: {', '.join(self.developers)}\033[0m".center(os.get_terminal_size().columns))
 
 def main():
+    ascii_art = """
+  _____ _     ____  ____   ___  ____   ____ 
+ |_   _| |   |  _ \|  _ \ / _ \|  _ \ / ___|
+   | | | |   | |_) | |_) | | | | |_) | |  _ 
+   | | | |___|  __/|  _ <| |_| |  _ <| |_| |
+   |_| |_____|_|   |_| \_\\___/|_| \_\\____|
+                                            """
 
-  ascii_art = """
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@@@@@@@@@#+=-::-=+#@@@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@@@@@@#+-::::::::::-*@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@@@%***=-::::::::::::=@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@#+-::::-+%%+-:::::::::-@@@@@@@@@@@@
-@@@@@@@@@@@@%+-:::::::::+@@=:::::::::=@@@@@@@@@@@
-@@@@@@@@@@#+=--=*%#=:::::::=@@+:::::::::=@@@@@@@@@@
-@@@@@@@%*+-::::-%@#:::::::+@@=:::::::::+@@@@@@@@@@
-@@@@@@@@@@@+::::-@@#:::::::*@@=:::::::::=@@@@@@@@@
-@@@@@@@@@@@@=::::=@@*:::::::*@@=:::::::::*@@@@@@@@
-@@@@@@@@@@@@@@+::::#@@*::::::#@@*-::::::::#@@@@@@@
-@@@@@@@@@@@@@@@#=---*@@@+-::::%@@@#+-::::-+@@@@@@@
-@@@@@@@@@@@@@@@@@@@@@@@@@@@%%%@@@@@@@@@@@@@@@@@@@@@
-@@@@##########@@##%@@@@@@%####%%@@@@%####%@@@@@@@@
-@@@@#@@@::#@@#@@=:+@@@@@@@::#@%--*@@+:=@@+:=@@@@@@
-@@@@@@@@::#@@@@@=:+@@@@@@@::#@@=:=@@+:+@@#::@@@@@@
-@@@@@@@@::#@@@@@=:+@@@@@@@::#%+=+@@@+:+%+-+%@@@@@@
-@@@@@@@@::#@@@@@=:+@@@@@@@::#@@@@@@@+:+@#::*@@@@@@
-@@@@@@@@%%%@@@@@%%%%%%%@@@%%%@@@@@@@%%%@@@@@%%@@@@
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-"""
+    my_company = CompanyInfo(
+        name="\033[97mTlprORG\033[0m",
+        details="\033[97mTlprOrganisation is a socially-conscious technology company using both non-profit and for-profit models to drive positive change through software.\033[0m",
+        founders=["\033[97mFounder1\033[0m", "\033[97mFounder2\033[0m"],
+        location="\033[97mIndia\033[0m",
+        developers=["\033[97msajadtlpr\033[0m", "\033[97mdev1\033[0m", "\033[97mdev2\033[0m"],
+        ascii_art=ascii_art
+    )
 
-  my_company = CompanyInfo(
-    # Details
-  )
-
-  my_company.display_info()
+    while True:
+        my_company.display_info()
+        # Display current time and date
+        print(f"\033[96m{time.strftime('%H:%M:%S %d-%m-%Y')}\033[0m".center(os.get_terminal_size().columns))
+        time.sleep(1)  # Update every second
 
 if __name__ == "__main__":
-  main()
+    main()
