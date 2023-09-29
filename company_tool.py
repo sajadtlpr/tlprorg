5980998f7789302de850fcf485977c5d7866b99b
sajad.tlpr <123149691+sajadtlpr@users.noreply.github.com>
2023-09-29 22:44:07 +0530

    Update company_tool.py

--- company_tool.py
+++ company_tool.py
@@ -29,15 +29,14 @@ def main():
    |_| |_____|_|   |_| \_\\___/|_| \_\\____|
    
                                             """
-
-    my_company = CompanyInfo(
-        name="\033[97mTlprORG\033[0m",
-        details = "\033[97mTlprOrganisation: A tech company using non-profit and for-profit models to drive positive change.\033[0m"
-        founders=["\033[97mABDULLA SAJAD\033[0m"],
-        location="\033[97mIndia\033[0m",
-        developers=["\033[97msajadtlpr\033],
-        ascii_art=ascii_art
-    )
+my_company = CompanyInfo(
+    name="\033[97mTlprORG\033[0m",
+    details="\033[97mTlprOrganisation: A tech company using non-profit and for-profit models to drive positive change.\033[0m",
+    founders=["\033[97mABDULLA SAJAD\033[0m"],
+    location="\033[97mIndia\033[0m",
+    developers=["\033[97msajadtlpr\033[0m"],
+    ascii_art=ascii_art
+)
 
     while True:
         my_company.display_info()
